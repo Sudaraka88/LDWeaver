ae9f48b57124d2ddcfd02b1546b3238e1a1f269d
Sudaraka88 <sudarakm@Sudarakas-MacBook-Pro.local>
2023-01-18 00:18:47 +1100

    init

---
+++ R/extractSNPs.R
@@ -0,0 +1,88 @@
+#' extractSNPs
+#'
+#' Function to extract SNPs from a multi-fasta alignment.
+#'
+#' @importFrom Rcpp sourceCpp
+#' @import Matrix
+#'
+#' @param aln_path path to multi fasta alignment
+#' @param gap_freq sites with a gap frequency >gap_greq will be dropped (default = 0.15)
+#' @param maf_freq sites with a minor allele frequency <maf_freq will be dropped (default = 0.01)
+#' @param method specify the filtering method 'relaxed' or 'default' (default = 'default')
+#'
+#' @return R list with SNPs in sparse format and additional parameters
+#'
+#' @examples
+#'
+#' aln_path <- system.file("extdata", "sample.aln", package = "BacGWESß")
+#' snp.dat <- call_ACGTN_sparse(aln_path, gap_freq = 0.15, maf_freq = 0.01, method = "default")
+#'
+#' @export
+call_ACGTN_sparse <- function(aln_path, gap_freq = 0.15, maf_freq = 0.01, method = "default"){
+  # Check inputs
+  if(!file.exists(aln_path)) stop(paste("Can't locate file", aln_path))
+
+  if(method == "default") {
+    filter = 0
+  } else if(method == "relaxed") {
+    filter = 1
+  } else {
+    warning("Unkown filtering method, using default...")
+    filter = 0
+  }
+
+  t0 = Sys.time()
+  # snp.data <- getSNP_Sites(aln_path, filter, gap_freq, maf_freq)
+  snp.data <- getACGTN_Sites(aln_path, filter, gap_freq, maf_freq)
+
+  g = snp.data$params$seq.length
+  nseq = snp.data$params$num.seqs
+  # print(paste("Done in", round(difftime(Sys.time(), t0, units = "secs"), 2), "s"))
+
+  if(g==-1) stop("Error! sequences are of different lengths!")
+  if(nseq==0) stop("File does not contain any sequences!")
+
+
+  seq.names <-  gsub("^>","",snp.data$params$seq.names)
+  nsnp = snp.data$params$num.snps
+
+  snp.matrix_A <- Matrix::sparseMatrix(i=snp.data$i_A,
+                                       j=snp.data$j_A,
+                                       x=as.logical(snp.data$x_A),
+                                       dims = c(nseq, nsnp),
+                                       dimnames = list(seq.names, snp.data$pos))
+
+  snp.matrix_C <- Matrix::sparseMatrix(i=snp.data$i_C,
+                                       j=snp.data$j_C,
+                                       x=as.logical(snp.data$x_C),
+                                       dims = c(nseq, nsnp),
+                                       dimnames = list(seq.names, snp.data$pos))
+
+  snp.matrix_G <- Matrix::sparseMatrix(i=snp.data$i_G,
+                                       j=snp.data$j_G,
+                                       x=as.logical(snp.data$x_G),
+                                       dims = c(nseq, nsnp),
+                                       dimnames = list(seq.names, snp.data$pos))
+
+  snp.matrix_T <- Matrix::sparseMatrix(i=snp.data$i_T,
+                                       j=snp.data$j_T,
+                                       x=as.logical(snp.data$x_T),
+                                       dims = c(nseq, nsnp),
+                                       dimnames = list(seq.names, snp.data$pos))
+
+  snp.matrix_N <- Matrix::sparseMatrix(i=snp.data$i_N,
+                                       j=snp.data$j_N,
+                                       x=as.logical(snp.data$x_N),
+                                       dims = c(nseq, nsnp),
+                                       dimnames = list(seq.names, snp.data$pos))
+
+  uqe = apply(snp.data$ACGTN_table>0, 1, function(x) as.numeric(x>0))
+  r = rowSums(uqe)
+
+  print(paste("Done in", round(difftime(Sys.time(), t0, units = "secs"), 2), "s"))
+
+  return(list(snp.matrix_A=Matrix::t(snp.matrix_A), snp.matrix_C=Matrix::t(snp.matrix_C),
+              snp.matrix_G=Matrix::t(snp.matrix_G), snp.matrix_T=Matrix::t(snp.matrix_T),
+              snp.matrix_N=Matrix::t(snp.matrix_N), g = g, nsnp = nsnp, nseq = nseq,
+              seq.names = seq.names, r = r, uqe = uqe, POS = as.numeric(snp.data$pos)))
+}
---
+++ src/getACGTNsites.cpp
@@ -0,0 +1,242 @@
+#include <Rcpp.h>
+#include "kseq.h"
+#include <iostream>
+#include <fcntl.h>
+#include <stdio.h>
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List getACGTN_Sites(std::string file, int filter, double gap_thresh, double maf_thresh) {
+  // filter = 0, default in spydrpick
+  // filter = 1, relaxed
+  // default gap_thresh = 0.15, maf_thresh = 0.01
+  Rcout << "Step 1: Extracting 'g' (seq_length): ";
+  int n = 0;
+  int l = 0;
+  kseq seq;
+  const char * f = file.c_str();
+  int fp_ = open(f, O_RDONLY);
+  FunctorRead r;
+  kstream<int, FunctorRead> ks_(fp_, r);
+  l = ks_.read(seq);
+  int seq_length = seq.seq.length(); // This is the first sequence, while loop reads from the second, re-run below?
+  close(fp_);
+  Rcout << seq_length << " bases \n";
+
+  Rcout << "Step 2: Getting allele counts \n";
+  int fp = open(f, O_RDONLY);
+  kstream<int, FunctorRead> ks(fp, r);
+  NumericMatrix allele_counts(5, seq_length);
+  while((l = ks.read(seq)) >= 0) {
+    if(seq.seq.length()!=seq_length) {
+      return List::create(Named("seq.length") = -1); // failed due to mismatching seq. lengths
+      break;
+    }
+    for(int j=0; j<seq_length; j++){
+      if((seq.seq[j]=='a') || (seq.seq[j]=='A')){
+        allele_counts(0, j) += 1;
+      } else if((seq.seq[j]=='c') || (seq.seq[j]=='C')){
+        allele_counts(1, j) += 1;
+      } else if((seq.seq[j]=='g') || (seq.seq[j]=='G')){
+        allele_counts(2, j) += 1;
+      } else if((seq.seq[j]=='t') || (seq.seq[j]=='T')){
+        allele_counts(3, j) += 1;
+      } else {
+        allele_counts(4, j) += 1;
+      }
+    }
+    n++;
+  }
+  close(fp);
+
+  Rcout << "Step 3: Performing SNP filtering: ";
+  // Rcout << n  << '\n';
+  // allele_counts contain info required to filter...
+  IntegerVector idx_ngss = IntegerVector::create(0, 1, 2, 3);
+  // LogicalVector filt(seq_length);
+  int chk_flag;
+  std::vector<int> POS; // holder for indices
+  //std::vector<int> consensus; // here we don't really care about the consensus
+  POS.reserve(2*10000);
+  //consensus.reserve(2*10000);
+
+  int n_snp = 0;
+  // to avoid repeated checking of filter technique, repeat whole chunk
+  if(filter==0){ // spydrpick default
+    int min_maf = n*maf_thresh;
+    // Rcout << min_maf << '\n';
+    for(int j = 0; j<seq_length; j++){
+      if ((POS.capacity() - 2*n_snp)<100){
+        POS.reserve( 2*POS.capacity() );
+        //    consensus.reserve(2*consensus.capacity());
+      }
+
+      chk_flag = 0;
+      for(int k = 0; k<4; k++){
+        if(allele_counts(k, j) > 0){ // we need at least one non-gap allele
+          chk_flag += 1;
+          if(chk_flag > 1){ // seems polymorphic
+            if(allele_counts(4, j)/n < gap_thresh){ // now check gap content
+              NumericVector snp = allele_counts(_, j);
+              snp = snp[idx_ngss];
+              std::sort(snp.begin(), snp.end());
+              if(snp[2] > min_maf){ // second largest non-gap element
+                POS.push_back(j+1);
+                //          consensus.push_back(which_max(allele_counts(_,j)));
+                // filt[j] = true;
+                n_snp += 1; // keep count of snps
+              }
+            }
+            chk_flag = 0;
+            break;
+          }
+        }
+      }
+    }
+  } else{ // relaxed
+    int min_maf = n*(1-maf_thresh);
+    for(int j = 0; j<seq_length; j++){
+      chk_flag = 0;
+      for(int k = 0; k<4; k++){
+        if ((POS.capacity() - 2*n_snp)<100){
+          POS.reserve( 2*POS.capacity() );
+          //  consensus.reserve(2*consensus.capacity());
+        }
+
+        if(allele_counts(k, j) > 0){ // we need at least one non-gap allele
+          chk_flag += 1;
+          if(chk_flag > 1){ // seems polymorphic
+            if(allele_counts(4, j)/n < gap_thresh){ // now check gap content
+              // NumericVector snp = allele_counts(_, j);
+              // snp = snp[idx_ngss];
+              // std::sort(snp.begin(), snp.end());
+              //Rcout << j << ":" <<  (1-(max(allele_counts(_,j))/n)) << ' ';
+              if( max(allele_counts(_,j)) <= min_maf){
+                POS.push_back(j+1);
+                //      consensus.push_back(which_max(allele_counts(_,j)));
+                // filt[j] = true;
+                n_snp += 1;
+              }
+            }
+            chk_flag = 0;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  Rcout << n_snp << " sites retained \n";
+  // Prepare the data for sparse matrix
+  Rcout << "Step 4: Building sparse mx \n";
+
+  Rcpp::StringVector seq_names(n);
+  n = 0;
+  int k; // indexing the positions
+  std::vector<int> m_i_A; m_i_A.reserve(2*n_snp);
+  std::vector<int> m_j_A; m_j_A.reserve(2*n_snp);
+  std::vector<int> m_x_A; m_x_A.reserve(2*n_snp);
+
+  std::vector<int> m_i_C; m_i_C.reserve(2*n_snp);
+  std::vector<int> m_j_C; m_j_C.reserve(2*n_snp);
+  std::vector<int> m_x_C; m_x_C.reserve(2*n_snp);
+
+  std::vector<int> m_i_G; m_i_G.reserve(2*n_snp);
+  std::vector<int> m_j_G; m_j_G.reserve(2*n_snp);
+  std::vector<int> m_x_G; m_x_G.reserve(2*n_snp);
+
+  std::vector<int> m_i_T; m_i_T.reserve(2*n_snp);
+  std::vector<int> m_j_T; m_j_T.reserve(2*n_snp);
+  std::vector<int> m_x_T; m_x_T.reserve(2*n_snp);
+
+  std::vector<int> m_i_N; m_i_N.reserve(2*n_snp);
+  std::vector<int> m_j_N; m_j_N.reserve(2*n_snp);
+  std::vector<int> m_x_N; m_x_N.reserve(2*n_snp);
+
+  NumericMatrix ACGTN_table(5,n_snp);
+
+  int fp2 = open(f, O_RDONLY);
+  kstream<int, FunctorRead> ks2(fp2, r);
+  char temp_char;
+
+  // std::string nt ("AaCcGgTt");
+  // we need to add the functionality to find *R* and *UQE* required for MI computation
+
+  while((l = ks2.read(seq)) >= 0) {
+    // Record sequence names
+    seq_names[n] = seq.name;
+    k = 1;
+    // Rcout << seq_names[n]   << '\n';
+
+    for(int j : POS){
+      temp_char = seq.seq[j-1];
+      // Rcout << j << ':' << temp_char << ' ';
+      if((temp_char=='A') || (temp_char=='a')) { // && (consensus[k-1]!=0))){ // A = 0
+        m_i_A.push_back(n+1); // n+1 for base 1 indexing in R
+        // m_j.push_back(j); // j's are from POS - based 1 R idx already applied
+        m_j_A.push_back(k);
+        m_x_A.push_back(1); // 'A' but different from reference (is this what we need?)
+        ++ACGTN_table(0, (k-1));
+      } else if((temp_char=='C') || (temp_char=='c')) {// && (consensus[k-1]!=1))){ // C = 1
+        m_i_C.push_back(n+1);
+        // m_j.push_back(j);
+        m_j_C.push_back(k);
+        m_x_C.push_back(2);
+        ++ACGTN_table(1, (k-1));
+      } else if((temp_char=='G') || (temp_char=='g')) { //}&& (consensus[k-1]!=2))){ //G = 2
+        m_i_G.push_back(n+1);
+        // m_j.push_back(j);
+        m_j_G.push_back(k);
+        m_x_G.push_back(3);
+        ++ACGTN_table(2, (k-1));
+      } else if((temp_char=='T') || (temp_char=='t')) {//}&& (consensus[k-1]!=3))){ // T = 3
+        m_i_T.push_back(n+1);
+        // m_j.push_back(j);
+        m_j_T.push_back(k);
+        m_x_T.push_back(4);
+        ++ACGTN_table(3, (k-1));
+      } else if((temp_char=='N') || (temp_char=='n')){ // not possible
+        m_i_N.push_back(n+1);
+        // m_j.push_back(j);
+        m_j_N.push_back(k);
+        m_x_N.push_back(5);
+        ++ACGTN_table(4, (k-1));
+      }
+      k += 1; // SNP indexes
+    }
+    n += 1;
+  }
+  close(fp2);
+  // Rcout << n  << '\n';
+
+  List params = List::create(_["num.seqs"] = n,
+                             _["num.snps"] = n_snp,
+                             _["seq.names"] = seq_names,
+                             _["seq.length"] = seq_length);
+
+  return List::create(Named("pos") = wrap(POS),
+                      //Named("consensus") = wrap(consensus), // we don't need a consensus here
+                      Named("ACGTN_table") = wrap(ACGTN_table),
+                      Named("i_A") = wrap(m_i_A),
+                      Named("j_A") = wrap(m_j_A),
+                      Named("x_A") = wrap(m_x_A),
+                      Named("i_C") = wrap(m_i_C),
+                      Named("j_C") = wrap(m_j_C),
+                      Named("x_C") = wrap(m_x_C),
+                      Named("i_G") = wrap(m_i_G),
+                      Named("j_G") = wrap(m_j_G),
+                      Named("x_G") = wrap(m_x_G),
+                      Named("i_T") = wrap(m_i_T),
+                      Named("j_T") = wrap(m_j_T),
+                      Named("x_T") = wrap(m_x_T),
+                      Named("i_N") = wrap(m_i_N),
+                      Named("j_N") = wrap(m_j_N),
+                      Named("x_N") = wrap(m_x_N),
+                      Named("params") = params);
+  //Named("num.seqs") = n,
+  //Named("num.snps") = n_snp,
+  //Named("seq.names") = seq_names,
+  //Named("seq.length") = seq_length);
+
+}
