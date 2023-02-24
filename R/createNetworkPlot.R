170e7012b0b0b2b16e735baf3ceb44ec68fb2473
Sudaraka88 <sudarakm@Sudarakas-MacBook-Pro.local>
2023-02-25 01:52:25 +1100

    bugfx

--- R/createNetworkPlot.R
+++ R/createNetworkPlot.R
@@ -2,16 +2,20 @@
 #'
 #' Function to generate a network view of top GWES hits
 #'
-#' @importFrom htmlwidgets saveWidget
-#' @importFrom stats cutree hclust dist
-#' @importFrom genbankr cds
 #' @importFrom plyr . ddply
-#' @importFrom chromoMap chromoMap
+#' @importFrom stats quantile
+#' @importFrom igraph graph_from_edgelist
+#' @importFrom intergraph asNetwork
+#' @importFrom network set.edge.attribute `%e%`
+#' @importFrom GGally ggnet2
+#' @importFrom ggplot2 ggsave
 #'
 #' @param srlinks_tophits data frame with top short range GWES links, returned from BacGWES::perform_snpEff_annotations()
 #' @param netplot_path folder to save the network
 #' @param separator break pattern for annotation (default = ":", which is the default used by SnpEff)
-#'
+#' @param max_plot_nodes specify the maximum number of nodes to visualise in the plot (default = 50). Large values will result in a cluttered output, consider increasing plot size.
+#' @param plot_w specify plot width in pixels (default = 6000)
+#' @param plot_h specify plot height in pixels (default = 4000)
 #' @return none
 #'
 #' @examples
@@ -20,7 +24,7 @@
 #' }
 #'
 #' @export
-create_network = function(srlinks_tophits, netplot_path, separator = ":", max_plot_nodes = 50){
+create_network = function(srlinks_tophits, netplot_path, separator = ":", max_plot_nodes = 50, plot_w = 6000, plot_h = 4000){
   # Let's ensure 50 nodes are present here
   c50 = T
   avail_links = max_plot_nodes
@@ -36,6 +40,7 @@ create_network = function(srlinks_tophits, netplot_path, separator = ":", max_pl
 
     if(nrow(df_uq) >= max_plot_nodes) c50=F
     avail_links = avail_links + 1
+    if(avail_links > nrow(srlinks_tophits)) c50=F
   }
   # get the max srp for each link
   for(i in 1:nrow(df_uq)){
@@ -44,14 +49,19 @@ create_network = function(srlinks_tophits, netplot_path, separator = ":", max_pl
 
   # swapping positions might improve the way the plot looks
   swp_lr = which(sapply(df_uq$p1a, function(x) x %in% df_uq$p2a))
-  tmp = df_uq$p1a[swp_lr]
-  df_uq$p1a[swp_lr] = df_uq$p2a[swp_lr]
-  df_uq$p2a[swp_lr] = tmp
+  if(length(swp_lr) > 0){
+    tmp = df_uq$p1a[swp_lr]
+    df_uq$p1a[swp_lr] = df_uq$p2a[swp_lr]
+    df_uq$p2a[swp_lr] = tmp
+  }
 
   swp_rl = which(sapply(df_uq$p2a, function(x) x %in% df_uq$p1a))
-  tmp = df_uq$p1a[swp_rl]
-  df_uq$p1a[swp_rl] = df_uq$p2a[swp_rl]
-  df_uq$p2a[swp_rl] = tmp
+  if(length(swp_rl) > 0){
+    tmp = df_uq$p1a[swp_rl]
+    df_uq$p1a[swp_rl] = df_uq$p2a[swp_rl]
+    df_uq$p2a[swp_rl] = tmp
+  }
+
 
   # some links might be the same, merge them
   pst1 = paste(df_uq$p1a, df_uq$p2a)
@@ -89,18 +99,18 @@ create_network = function(srlinks_tophits, netplot_path, separator = ":", max_pl
   g1 = network::set.edge.attribute(x = g1, attrname = "weights", value = s_weights)
 
   # add colours
-  qs = quantile(s_weights)
+  qs = stats::quantile(s_weights)
   g1 = network::set.edge.attribute(x = g1, attrname = "color", value = ifelse(g1 %e% "weights" < qs[2], "grey75",
                                                                               ifelse(g1 %e% "weights" < qs[3], "grey50",
                                                                                      ifelse (g1 %e% "weights" < qs[4], "black", "red"))))
 
   g1 = network::set.edge.attribute(x = g1, attrname = "labs", value = s_labs)
 
-  p1 = ggnet2(g1, label = T, edge.size = "weights", edge.color = "color", label.color = "blue4",
+  p1 = GGally::ggnet2(g1, label = T, edge.size = "weights", edge.color = "color", label.color = "blue4",
               node.color = "aliceblue", node.size = 6, shape = 13, edge.label = "labs", edge.label.color = "darkorchid1",
               label.size = 4, mode = "fruchtermanreingold")
 
   # p1
-  ggsave(plot = p1, filename = netplot_path, width = 6000, height = 4000, units = "px", device = "png", dpi = 300)
+  ggplot2::ggsave(plot = p1, filename = netplot_path, width = 6000, height = 4000, units = "px", device = "png", dpi = 300)
 
 }
